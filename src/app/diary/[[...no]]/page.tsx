d22affb1e683e727dbb6e46652874c6599f6cc77
yun-sangho <contact@yunsangho.com>
2024-02-09 19:22:54 +0900

    reshape layout

--- src/app/diary/[[...no]]/page.tsx
+++ src/app/diary/[[...no]]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { CircularProgress, Stack, Typography } from '@mui/material';
+import { CircularProgress, Drawer, Stack, Typography } from '@mui/material';
 import { Timer } from '@/components/diary/Timer';
 import { Navigation } from '@/components/diary/Navigation';
 import { Memo } from '@/components/diary/Memo';
@@ -31,18 +31,26 @@ export default function SpeakingDiary({
 
   return (
     <Stack height={'100%'} justifyContent={'space-between'}>
-      <Stack my={4} gap={1}>
+      <Stack my={2} gap={2}>
+        <Timer />
         <Stack maxHeight={130} overflow={'auto'}>
-          <Typography variant={'h5'} fontWeight={700}>
+          <Typography
+            fontSize={20}
+            fontWeight={600}
+            sx={{
+              lineHeight: 1.2,
+            }}
+          >
             {store.currentIdx}. {store.getQuestion()}
           </Typography>
         </Stack>
+
         <Memo />
-        <Timer />
       </Stack>
       <Stack gap={2}>
         <Navigation />
       </Stack>
+      {/*<Drawer anchor={'bottom'} open={true}></Drawer>*/}
     </Stack>
   );
 }
--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -14,9 +14,6 @@ export function MenuBar() {
         >
           <MenuIcon />
         </IconButton>
-        <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
-          English
-        </Typography>
       </Toolbar>
     </AppBar>
   );
